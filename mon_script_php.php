5c2ad425fc67ca79c1faf5ef5303b97c85b0dc9d
«votre <danielbeaudet_750@hotmail.com>
2017-03-01 13:32:53 -0500

    Supression du code et ajout de la date pour php

--- mon_script_php.php
+++ mon_script_php.php
@@ -1,4 +1,11 @@
-<?php
-
-echo '<h1>Bonjour à tous</h1>';
-echo '<p>Ça va ?</p>';
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Ma page web</title>
+    </head>
+    <body>
+        <h1>Ma page web</h1>
+        <p>Aujourd'hui nous sommes le <?php echo date("d/m/Y h:i:s"); ?>.</p>
+    </body>
+</html>
