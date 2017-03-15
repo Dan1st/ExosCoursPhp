a24631af8157408757d9a4a930f889f36f08ff35
«votre <danielbeaudet_750@hotmail.com>
2017-03-15 10:30:42 -0400

    Modification de la requête pour que seulement les noms de jeux vidéo soient affichés

--- connexion.php
+++ connexion.php
@@ -36,17 +36,14 @@
             // Si tout va bien, on peut continuer
 
             // On récupère tout le contenu de la table jeux_video
-            $reponse = $bdd->query('SELECT * FROM jeux_video');
+            $reponse = $bdd->query('SELECT nom FROM jeux_video');
 
             // On affiche chaque entrée une à une
             while ($donnees = $reponse->fetch())
             {
             ?>
                 <p>
-                <strong>Jeu</strong> : <?php echo $donnees['nom']; ?><br />
-                Le possesseur de ce jeu est : <?php echo $donnees['possesseur']; ?>, et il le vend à <?php echo $donnees['prix']; ?> euros !<br />
-                Ce jeu fonctionne sur <?php echo $donnees['console']; ?> et on peut y jouer à <?php echo $donnees['nbre_joueurs_max']; ?> au maximum<br />
-                <?php echo $donnees['possesseur']; ?> a laissé ces commentaires sur <?php echo $donnees['nom']; ?> : <em><?php echo $donnees['commentaires']; ?></em>
+                <strong>Jeu</strong> : <?php echo $donnees['nom']; ?>
                </p>
             <?php
             }
