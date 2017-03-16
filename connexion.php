08cd292114bae9cb12f7616113bebb3f59e18fca
«votre <danielbeaudet_750@hotmail.com>
2017-03-16 12:21:04 -0400

    Remplacement du nom Michel pour Florent

--- connexion.php
+++ connexion.php
@@ -32,7 +32,7 @@
             }
 
             // On ajoute une entrée dans la table jeux_video
-            $bdd->exec("UPDATE jeux_video SET prix = '10', nbre_joueurs_max = '32' WHERE nom = 'Battlefield 1942'");
+            $bdd->exec("UPDATE jeux_video SET possesseur = 'Florent' WHERE possesseur = 'Michel'");
 
             echo 'Le jeu a bien été modifé !';
         ?>
