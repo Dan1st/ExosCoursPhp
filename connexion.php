16503d64d66ca3d0669ed7b75366ab5688c138a8
«votre <danielbeaudet_750@hotmail.com>
2017-03-16 12:18:16 -0400

    Modification du prix (10 $) et du nombre de joueurs (32) du jeu Battlefield

--- connexion.php
+++ connexion.php
@@ -21,21 +21,21 @@
 
         <!-- Add your site or application content here -->
         <?php
-        try
-        {
-            $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-            $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
-        }
-        catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+                $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION );
+            }
+            catch(Exception $e)
+            {
+                    die('Erreur : '.$e->getMessage());
+            }
 
-// On ajoute une entrée dans la table jeux_video
-$bdd->exec('INSERT INTO jeux_video(nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES(\'Battlefield 1942\', \'Patrick\', \'PC\', 45, 50, \'2nde guerre mondiale\')');
+            // On ajoute une entrée dans la table jeux_video
+            $bdd->exec("UPDATE jeux_video SET prix = '10', nbre_joueurs_max = '32' WHERE nom = 'Battlefield 1942'");
 
-echo 'Le jeu a bien été ajouté !';
-?>
+            echo 'Le jeu a bien été modifé !';
+        ?>
 
         <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
